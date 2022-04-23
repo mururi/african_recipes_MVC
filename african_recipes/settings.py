943a1f6dd34d9e1a47464b31acf2008023dcb354
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-23 22:02:54 +0300

    Setup cloudinary

--- african_recipes/settings.py
+++ african_recipes/settings.py
@@ -14,6 +14,10 @@ import dj_database_url
 from pathlib import Path
 from decouple import config, Csv
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 MODE = config('MODE', default="dev")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
     'bootstrap5',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -144,3 +149,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config( 
+  cloud_name = config('CLOUDINARY_CLOUD_NAME'), 
+  api_key = config('CLOUDINARY_API_KEY'), 
+  api_secret = config('CLOUDINARY_API_SECRET'),
+  secure = config('CLOUDINARY_SECURE')
+)
