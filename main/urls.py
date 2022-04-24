e402b1a484716999937d9581295013de5447201e
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-04-24 03:37:47 +0300

    Add update view function, url and template

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add-recipe/', views.add_recipe, name='add-recipe'),
-    path('recipe/<int:pk>/', views.view_recipe, name='view-recipe')
+    path('recipe/<int:pk>/', views.view_recipe, name='view-recipe'),
+    path('update-recipe/<int:pk>/', views.update_recipe, name='update-recipe'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.models import Recipe
 from .forms import RecipeForm
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def home(request):
     recipes = Recipe.get_recipes()
@@ -22,7 +24,7 @@ def add_recipe(request):
     context = {
         "form": form
     }
-    return render(request, 'add-recipe.html', context)
+    return render(request, 'recipe-form.html', context)
 
 def view_recipe(request, pk):
     recipe = Recipe.get_recipe_by_id(pk)
@@ -31,3 +33,18 @@ def view_recipe(request, pk):
         "recipe": recipe
     }
     return render(request, 'view-recipe.html', context)
+
+def update_recipe(request, pk):
+    recipe = Recipe.objects.get(id = pk)
+    form = RecipeForm(instance=recipe)
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+        return redirect(reverse('view-recipe', args=[recipe.id]))
+    
+    context = {
+        "form": form,
+        "recipe": recipe
+    }
+    return render(request, 'update-recipe.html', context)
